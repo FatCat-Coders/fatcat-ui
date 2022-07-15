b2b1d2004d04225714ca9416f1e307a04ef12534
automateeverythingM <marko.pavic@fatcatcoders.com>
2022-07-15 17:28:34 +0200

    fix: fix line height

--- src/theme/styles/text.ts
+++ src/theme/styles/text.ts
@@ -3,59 +3,59 @@ import { css } from 'styled-components';
 export const TEXT_SIZE = {
 	h1: css`
 		font-size: ${props => props.theme.fontSize.s48};
-		line-height: ${props => props.theme.lineHeight.s170};
+		line-height: ${props => props.theme.lineHeight.s150};
 	`,
 	h1Tablet: css`
 		font-size: ${props => props.theme.fontSize.s42};
-		line-height: ${props => props.theme.lineHeight.s125};
+		line-height: ${props => props.theme.lineHeight.s150};
 	`,
 	h1Mobile: css`
 		font-size: ${props => props.theme.fontSize.s34};
-		line-height: ${props => props.theme.lineHeight.s148};
+		line-height: ${props => props.theme.lineHeight.s150};
 	`,
 	h2: css`
 		font-size: ${props => props.theme.fontSize.s34};
-		line-height: ${props => props.theme.lineHeight.s151};
+		line-height: ${props => props.theme.lineHeight.s150};
 	`,
 	h2Mobile: css`
 		font-size: ${props => props.theme.fontSize.s24};
-		line-height: ${props => props.theme.lineHeight.s136};
+		line-height: ${props => props.theme.lineHeight.s150};
 	`,
 	h3: css`
 		font-size: ${props => props.theme.fontSize.s24};
-		line-height: ${props => props.theme.lineHeight.s136};
+		line-height: ${props => props.theme.lineHeight.s150};
 	`,
 	h3Mobile: css`
 		font-size: ${props => props.theme.fontSize.s20};
-		line-height: ${props => props.theme.lineHeight.s130};
+		line-height: ${props => props.theme.lineHeight.s150};
 	`,
 	h4: css`
 		font-size: ${props => props.theme.fontSize.s20};
-		line-height: ${props => props.theme.lineHeight.s130};
+		line-height: ${props => props.theme.lineHeight.s150};
 	`,
 	h4Mobile: css`
 		font-size: ${props => props.theme.fontSize.s18};
-		line-height: ${props => props.theme.lineHeight.s127};
+		line-height: ${props => props.theme.lineHeight.s150};
 	`,
 	h5: css`
 		font-size: ${props => props.theme.fontSize.s16};
-		line-height: ${props => props.theme.lineHeight.s124};
+		line-height: ${props => props.theme.lineHeight.s150};
 	`,
 	h5Mobile: css`
 		font-size: ${props => props.theme.fontSize.s16};
-		line-height: ${props => props.theme.lineHeight.s124};
+		line-height: ${props => props.theme.lineHeight.s150};
 	`,
 	h6: css`
 		font-size: ${props => props.theme.fontSize.s14};
-		line-height: ${props => props.theme.lineHeight.s121};
+		line-height: ${props => props.theme.lineHeight.s150};
 	`,
 	h6Mobile: css`
 		font-size: ${props => props.theme.fontSize.s14};
-		line-height: ${props => props.theme.lineHeight.s121};
+		line-height: ${props => props.theme.lineHeight.s150};
 	`,
 	paragraph: css`
 		font-size: ${props => props.theme.fontSize.s24};
-		line-height: ${props => props.theme.lineHeight.s136};
+		line-height: ${props => props.theme.lineHeight.s150};
 	`,
 	paragraphTablet: css`
 		font-size: ${props => props.theme.fontSize.s18};
