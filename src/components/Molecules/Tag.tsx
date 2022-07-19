2f12e263cc05c148c6dfb1a64f4cd91cc2b3176f
automateeverythingM <marko.pavic@fatcatcoders.com>
2022-07-19 11:37:44 +0200

    fix: add svg to tag

--- src/components/Molecules/Tag.tsx
+++ src/components/Molecules/Tag.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// Atoms
-import { Image } from '../Atoms/Image';
-
-// Icons
-import close from '../../icons/close.svg';
-
 export const TagWrapper = styled.div`
 	display: flex;
 	flex-wrap: nowrap;
@@ -25,7 +19,9 @@ export const TagWrapper = styled.div`
 	}
 
 	& > button {
-        cursor: pointer;
+		display:inline-flex;
+		align-items:center;
+		cursor: pointer;
 		border: none;
 		padding: 0;
 		margin-left: ${props => props.theme.space.s8};
@@ -43,8 +39,24 @@ export const Tag = (props: TTagProps) => {
 		<TagWrapper>
 			<p>{tagName}</p>
 			{onClose && (
-				<button type="button" aria-roledescription="button" onClick={onClose}>
-					<Image src={close} alt="close" />
+				<button
+					type="button"
+					aria-roledescription="button"
+					onClick={onClose}
+				>
+					<svg
+						width="16"
+						height="16"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="m8.5 7.097 2.806-2.806a.992.992 0 1 1 1.403 1.403L9.903 8.5l2.806 2.806a.992.992 0 1 1-1.403
+							1.403L8.5 9.903 5.694 12.71a.992.992 0 1 1-1.403-1.403L7.097 8.5 4.29 5.694A.992.992 0 1 1 5.694
+							4.29L8.5 7.097Z"
+							fill="#B8BBC5"
+						/>
+					</svg>
 				</button>
 			)}
 		</TagWrapper>
