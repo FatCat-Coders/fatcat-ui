0e100322a7fdc63b1acd0300899ecbfe8a959e83
Branislav Totic <branislav.totic@gmail.com>
2022-07-19 12:36:24 +0200

    fix: divider component

--- src/components/Atoms/Divider.tsx
+++ src/components/Atoms/Divider.tsx
@@ -6,7 +6,20 @@ export const DottedDivider = styled('hr')<{ color?: string }>`
 	width: 100%;
 	height: 2px;
 	margin: 0;
+	background-color: transparent;
 	background-image: ${props => `linear-gradient(to right, ${props.color ? props.theme.color[props.color] : '#000000'} 10%, rgba(255, 255, 255, 0) 0%)`};
+	background-position: top;
+	background-size: 10px 1px;
+	background-repeat: repeat-x;
+`;
+
+export const DashedDivider = styled('hr')<{ color?: string }>`
+	border: none;
+	width: 100%;
+	height: 2px;
+	margin: 0;
+	background-color: transparent;
+	background-image: ${props => `linear-gradient(to right, ${props.color ? props.theme.color[props.color] : '#000000'} 40%, rgba(255, 255, 255, 0) 0%)`};
 	background-position: top;
 	background-size: 10px 1px;
 	background-repeat: repeat-x;
