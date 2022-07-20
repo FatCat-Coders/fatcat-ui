579aefea5b9f071dca5ba8f9a97284deae8b3532
automateeverythingM <marko.pavic@fatcatcoders.com>
2022-07-20 14:28:52 +0200

    feat: add storybook and fix types

--- src/components/Molecules/Tag/Tag.atoms.tsx
+++ src/components/Molecules/Tag/Tag.atoms.tsx
@@ -1,17 +1,17 @@
-import { Properties } from 'csstype';
 import { styled } from '../../../utils/styled';
+import { DefaultTheme } from 'styled-components';
 
 export type TTagWrapper = {
-	background?: Properties['background'];
-	textColor?: Properties['color'];
+	background?: keyof DefaultTheme['color'];
+	textColor?: keyof DefaultTheme['textColor'];
 };
 
 export const TagWrapper = styled('div')<TTagWrapper>`
 	display: flex;
 	flex-wrap: nowrap;
 	align-items: center;
-	color: ${props => props.textColor || props.theme.textColor.secondary};
-	background-color:${props => props.background || props.theme.color.lightGrey};
+	color: ${props => props.theme.color[props.textColor]};
+	background:${props => props.background || props.theme.color.lightGrey};
 	padding: ${props => props.theme.space.s8} ${props => props.theme.space.s10};
 	width: auto;
 	text-align: center;
@@ -33,3 +33,8 @@ export const TagWrapper = styled('div')<TTagWrapper>`
 		background-color: inherit;
 	}
 `;
+
+TagWrapper.defaultProps = {
+	background: 'lightGrey',
+	textColor: 'secondary',
+};
