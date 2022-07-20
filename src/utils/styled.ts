633332f250a51e498feed99484f3c229c22f9db5
Branislav Totic <branislav.totic@gmail.com>
2022-07-20 14:49:41 +0200

    fix: tag storybook

--- src/utils/styled.ts
+++ src/utils/styled.ts
@@ -3,6 +3,38 @@ import { default as styl } from 'styled-components'; // eslint-disable-line
 // before adding new props to UI-components, check list if it's included here:
 // https://github.dev/styled-components/styled-components/tree/main/packages/styled-components
 export const UIprops: string[] = [
+	// animation props
+	'animation',
+	'animationName',
+	'animationDuration',
+	'animationTimingFunction',
+	'animationDelay',
+	'animationIterationCount',
+	'animationDirection',
+	'animationFillMode',
+	'animationPlayState',
+	// background props
+	'background',
+	'backgroundColor',
+	'backgroundColorOpacity',
+	'backgroundImage',
+	'backgroundRepeat',
+	'backgroundPosition',
+	'backgroundSize',
+	'backgroundGradient',
+	'backgroundBlur',
+	'backgroundAttachment',
+	'backgroundBlendMode',
+	// border props
+	'border',
+	'borderTop',
+	'borderRight',
+	'borderBottom',
+	'borderLeft',
+	'borderRadius',
+	'boxShadow',
+	// flex props
+	'flex',
 	'direction',
 	'wrap',
 	'overflow',
