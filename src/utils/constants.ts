32012bac1c0eca5a046affab978d959f480a1857
Branislav Totic <branislav.totic@gmail.com>
2022-10-17 10:56:24 +0200

    feat: add more options for Contentful options constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,18 @@
 export const CONTENTFUL_OPTIONS = {
 	PADDING: 'padding',
+	PADDING_TOP: 'paddingTop',
+	PADDING_RIGHT: 'paddingRight',
+	PADDING_BOTTOM: 'paddingBottom',
+	PADDING_LEFT: 'paddingLeft',
+	PADDING_Y: 'paddingY',
+	PADDING_X: 'paddingX',
 	MARGIN: 'margin',
+	MARGIN_TOP: 'marginTop',
+	MARGIN_RIGHT: 'marginRight',
+	MARGIN_BOTTOM: 'marginBottom',
+	MARGIN_LEFT: 'marginLeft',
+	MARGIN_Y: 'marginY',
+	MARGIN_X: 'marginX',
 	OVERFLOW: 'overflow',
 
 	// Responsive
