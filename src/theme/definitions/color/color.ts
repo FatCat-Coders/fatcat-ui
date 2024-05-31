1ab97b920bcfcfa217e7d3f1b2f47ce9e5292f72
Ivana-Jovic <ivana.n.jovic@gmail.com>
2024-05-31 13:59:42 +0200

    feat: added new colors

--- src/theme/definitions/color/color.ts
+++ src/theme/definitions/color/color.ts
@@ -31,8 +31,82 @@ export const COLOR = {
 	disabled: '#E0E2EC',
 	disabledDark: '#A0A3AF',
 	warning: '#DE3131',
-	primary600: '#5C62F9',
+	///
+	// brand/primary
+	primary50: '#F9FAFF',
+	primary100: '#EBECFE',
+	primary200: '#D8D9FE',
+	primary300: '#B0B3FC',
+	primary400: '#898DFB',
+	primary500: '#757AFA',
+	primary600: '#5C62F9', // base
+	primary700: '#4E54F9',
+	primary800: '#3A40F8',
+	ywllow900: '#262DF7',
+	// brand/secondary
+	// yellow
+	yellow50: '#FFFBEB',
+	yellow100: '#FFF7D6',
+	yellow200: '#FFF3C2',
+	yellow300: '#FFEFAD',
+	yellow400: '#FFE785',
+	yellow500: '#FFDE5C',
+	yellow600: '#FFD633',
+	yellow700: '#FFCC00', // base
+	yellow800: '#F5C400',
+	yellow900: '#E0B400',
+	// pink
+	pink50: '#FFF6F7',
+	pink100: '#FFF0F1',
+	pink200: '#FFE5E7',
+	pink300: '#FDD8DB',
+	pink400: '#FCC5C8',
+	pink500: '#FA9EA4',
+	pink600: '#F97F88',
+	pink700: '#F9777F', // base
+	pink800: '#F8636D',
+	pink900: '#F7505B',
+	// mint
+	mint50: '#EFFBF7',
+	mint100: '#DFF6F0',
+	mint200: '#CFF2E8',
+	mint300: '#AEEAD9',
+	mint400: '#AEEAD9',
+	mint500: '#4ED0AB',
+	mint600: '#35C89F', // base
+	mint700: '#2FB18C',
+	mint800: '#2BA180',
+	mint900: '#228166',
+	// neutrals
+	cream: '#F9F6F3',
+	neutrals50: '#FFFFFF',
+	neutrals100: '#F2F3F5',
+	neutrals200: '#E6E6EA',
+	neutrals300: '#CCCED5',
+	neutrals400: '#999CAB',
+	neutrals500: '#676B81',
+	neutrals600: '#343957',
+	neutrals700: '#2E324D',
 	neutrals800: '#01082D',
+	// feedback/semantics
+	// success
+	successDark: '#067647',
+	successMedium: '#079455',
+	successLight: '#ECFDF3',
+	// error
+	errorExtraDark: '#B42318',
+	errorDark: '#D92D20',
+	errorMedium: '#F04438',
+	errorLight: '#FEF3F2',
+	// warning
+	warningDark: '#B54708',
+	warningMedium: '#DC6803',
+	warningLight: '#FEF0C7',
+	// info
+	infoExtraDark: '#0E6BAA',
+	infoDark: '#0F77BD',
+	infoMedium: '#1D99ED',
+	infoLight: '#D9EEFC',
 };
 
 export const BUTTON_COLOR = {
