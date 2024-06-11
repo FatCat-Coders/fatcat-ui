1621b2884d53bfdf6896623af90575adef3ddbd9
Aleksa Stevic <aleksa.stevic@fatcatcoders.com>
2024-06-11 18:45:01 +0200

    [FCC-433] update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,45 +1,6 @@
-/// <reference types="react" />
-/// <reference types="react-dom" />
-declare module '*.svg' {
-	const src: string;
-	export default src;
-}
+import { FatCatTheme } from './theme/theme';
 
-declare module '*.md' {
-	const src: string;
-	export default src;
-}
-declare module '*.bmp' {
-	const src: string;
-	export default src;
-}
-
-declare module '*.gif' {
-	const src: string;
-	export default src;
-}
-
-declare module '*.jpg' {
-	const src: string;
-	export default src;
-}
-
-declare module '*.jpeg' {
-	const src: string;
-	export default src;
-}
-
-declare module '*.png' {
-	const src: string;
-	export default src;
-}
-
-declare module '*.webp' {
-	const src: string;
-	export default src;
-}
-
-declare module '*.ico' {
-	const src: string;
-	export default src;
+declare module 'styled-components' {
+	// eslint-disable-next-line
+	export interface DefaultTheme extends FatCatTheme {}
 }
--- src/other.d.ts
+++
@@ -1,6 +0,0 @@
-import { FatCatTheme } from './theme/theme';
-
-declare module 'styled-components' {
-	// eslint-disable-next-line
-	export interface DefaultTheme extends FatCatTheme {}
-}
